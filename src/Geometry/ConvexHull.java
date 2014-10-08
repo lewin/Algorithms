dfbacb6e7d943edc6c5203717724946bab7f94ea
lewin <lg52931@berkeley.edu>
2014-10-07 23:07:18 -0700

    fix convex hull

--- src/Geometry/ConvexHull.java
+++ src/Geometry/ConvexHull.java
@@ -1,6 +1,7 @@
 package Geometry;
 
 import static Geometry.Utils.EPS;
+import static Geometry.Utils.cross;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -10,87 +11,69 @@ import java.util.Comparator;
  * Convex hull made easy
  * 
  * Adapted from USACO training pages
+ * 
  * @author Lewin
  */
 public class ConvexHull {
-    public static final double  EPS = 0.0000000001;
-    
-    /**
-     * Given an array of N unsorted points, returns an array of points that make up
-     * the convex hull of the points in clockwise order
-     * 
-     * The points are the same objects (NOT copies)
-     * 
-     * Uses the gift-wrapping algorithm. Runs in O(N log N) time
-     * @param p array of points
-     * @return array of points in convex hull in order
-     */
-    private static Point [] convexHull (Point [] p) {
-        int numPoints = p.length;
-        Point m = new Point (0, 0);
-        for (int i = 0; i < numPoints; i++) {
-            m.x += p[i].x / numPoints;
-            m.y += p[i].y / numPoints;
-        }
-        
-        for (int i = 0; i < numPoints; i++)
-            p[i].angle = Math.atan2 (p[i].y - m.y, p[i].x - m.x);
-        
-        Arrays.sort (p, new Comparator<Point> () {
-            public int compare (Point a, Point b) {
-                return (int)Math.signum (a.angle - b.angle);
-            }
-        });
-        
-        Point [] hull = new Point [numPoints];
-        hull[0] = p[0];
-        hull[1] = p[1];
-        
-        int hullPos = 2;
-        for (int i = 2; i < numPoints - 1; i++) {
-            while (hullPos > 1
-                    && cross (hull[hullPos - 2], hull[hullPos - 1], p[i]) < -EPS)
-                hullPos--;
-            hull[hullPos++] = p[i];
-        }
-        
-        // add last point
-        Point p2 = p[numPoints - 1];
-        while (hullPos > 1
-                && cross (hull[hullPos - 2], hull[hullPos - 1], p2) < -EPS)
-            hullPos--;
-        
-        int hullStart = 0;
-        boolean flag = false;
-        do {
-            flag = false;
-            if (hullPos - hullStart >= 2
-                    && cross (hull[hullStart], p2, hull[hullPos - 1]) > EPS) {
-                p2 = hull[--hullPos];
-                flag = true;
-            }
-            if (hullPos - hullStart >= 2
-                    && cross (p2, hull[hullStart + 1], hull[hullStart]) > EPS) {
-                hullStart++;
-                flag = true;
-            }
-        } while (flag);
-        hull[hullPos++] = p2;
-        
-        Point [] cleanHull = new Point [hullPos - hullStart];
-        for (int i = hullStart; i < hullPos; i++)
-            cleanHull[i - hullStart] = hull[i];
-        
-        return cleanHull;
+  /**
+   * Given an array of N unsorted points, returns an array of points that make up the convex hull of
+   * the points in clockwise order
+   * 
+   * The points are the same objects (NOT copies)
+   * 
+   * Uses the gift-wrapping algorithm. Runs in O(N log N) time
+   * 
+   * @param p array of points
+   * @return array of points in convex hull in order
+   */
+  private static Point[] convexHull(Point[] p) {
+    int numPoints = p.length;
+    Point m = new Point(0, 0);
+    for (int i = 0; i < numPoints; i++) {
+      m.x += p[i].x / numPoints;
+      m.y += p[i].y / numPoints;
     }
-    
-    private static double zCrossProduct (Point a, Point b) {
-        return a.x * b.y - a.y * b.x;
+
+    for (int i = 0; i < numPoints; i++)
+      p[i].angle = Math.atan2(p[i].y - m.y, p[i].x - m.x);
+
+    Arrays.sort(p, new Comparator<Point>() {
+      public int compare(Point a, Point b) {
+        return (int) Math.signum(a.angle - b.angle);
+      }
+    });
+
+    Point[] hull = new Point[numPoints];
+    hull[0] = p[0];
+    hull[1] = p[1];
+
+    int hullPos = 2;
+    for (int i = 2; i < numPoints - 1; i++) {
+      while (hullPos > 1 && cross(hull[hullPos - 2], hull[hullPos - 1], p[i]) < EPS)
+        hullPos--;
+      hull[hullPos++] = p[i];
     }
-    
-    private static double cross (Point a, Point b, Point c) {
-        Point ab = new Point (a.x - b.x, a.y - b.y);
-        Point bc = new Point (b.x - c.x, b.y - c.y);
-        return zCrossProduct (ab, bc);
-    }
+
+    // add last point
+    Point p2 = p[numPoints - 1];
+    while (hullPos > 1 && cross(hull[hullPos - 2], hull[hullPos - 1], p2) < EPS)
+      hullPos--;
+
+    int hullStart = 0;
+    boolean flag = false;
+    do {
+      flag = false;
+      if (hullPos - hullStart >= 2 && cross(hull[hullPos - 1], p2, hull[hullStart]) < EPS) {
+        p2 = hull[--hullPos];
+        flag = true;
+      }
+      if (hullPos - hullStart >= 2 && cross(p2, hull[hullStart], hull[hullStart + 1]) < EPS) {
+        hullStart++;
+        flag = true;
+      }
+    } while (flag);
+    hull[hullPos++] = p2;
+
+    return Arrays.copyOfRange(hull, hullStart, hullPos);
+  }
 }
